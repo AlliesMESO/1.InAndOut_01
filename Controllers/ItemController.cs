6896fc4894cc5c4fd8fc0f3731d9070e9a09cf32
AlliesMESO <137116822+AlliesMESO@users.noreply.github.com>
2023-08-01 13:07:04 +0200

    #Section 4: Model,DataBase and CRUD 27. Setting Up The Connectionstring 28. Setting up The DbContext as well as EntityFrameWork 29. Setting Up the DB Context in Our Startup File 30. Push Our Items To The Database, Micrations and Checking the DB 31. Add A Controller For Our Borrowed Items 32. Passing Data To The Controller

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,23 @@
+﻿using _1.InAndOut_01.Data;
+using _1.InAndOut_01.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace _1.InAndOut_01.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ItemController(ApplicationDbContext db) 
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Item> objList = _db.Items;
+            return View(objList);
+        }
+    }
+}
