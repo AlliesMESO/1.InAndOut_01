7f1169575c5beb377034dc0564442321579c1bd2
AlliesMESO <137116822+AlliesMESO@users.noreply.github.com>
2023-08-04 02:04:59 +0200

    51. ViewModels in Action

--- Controllers/ExpenseController.cs
+++ Controllers/ExpenseController.cs
@@ -1,5 +1,6 @@
 ﻿using InAndOut.Data;
 using InAndOut.Models;
+using InAndOut.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
@@ -25,24 +26,35 @@ namespace InAndOut.Controllers
         //GET-Create
         public IActionResult Create()
         {
-            IEnumerable<SelectListItem> TypeDropDown = _db.ExpenseTypes.Select(i => new SelectListItem
+            /*IEnumerable<SelectListItem> TypeDropDown = _db.ExpenseTypes.Select(i => new SelectListItem
             {
                 Text = i.Name,
                 Value = i.Id.ToString()
             });
-            ViewBag.TypeDropDown = TypeDropDown;
-            return View();
+            ViewBag.TypeDropDown = TypeDropDown;*/
+
+            ExpenseVM expenseVM = new ExpenseVM()
+            {
+                Expense = new Expense(),
+                TypeDropDown = _db.ExpenseTypes.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                })
+            };
+        
+            return View(expenseVM);
         }
 
         //POST-Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Expense obj)
+        public IActionResult Create(ExpenseVM obj)
         {
             if (ModelState.IsValid)
             {
-                obj.ExpenseTypeId = 1;
-                _db.Expenses.Add(obj);
+                //obj.ExpenseTypeId = 1;
+                _db.Expenses.Add(obj.Expense);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
